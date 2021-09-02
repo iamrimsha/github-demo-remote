b0834435a2c5b33e655badec8fbb40a5c5942e93
iamrimsha <89963752+iamrimsha@users.noreply.github.com>
2021-09-02 12:51:24 +0530

    "Initial commit of math.py file"

---
+++ math.py
@@ -0,0 +1,20 @@
+#ADD
+def add(x,y):
+ pass
+ 
+ #SUB
+ def sub(x,y):
+ return x-y 
+ 
+ #MUL
+ def mul(x,y):
+  return x*y
+ 
+ #DIV
+ def div(x,y):
+ pass
+
+#Square 
+ def square(x):
+  return x*x
+ 
